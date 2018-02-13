aa9676e7eb2f48ef92da3d59138769a520452bac
Greg Garville Jr <gjgarvil@gmail.com>
2018-02-13 13:48:27 -0500

    added dynamic seo_keywords method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  before_action :set_title
-  def set_title
+  before_action :set_page_defaults
+  def set_page_defaults
     @page_title = "Risk On vs Risk Off"
+    @seo_keywords "Greg Garville Asset"
   end
+
 
 end
---
+++ app/controllers/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+
+  # indcluded do
+  #   before_action :set_title
+  # end
+  #
+  # def set_title
+  #   @page_title = "Risk On vs Risk Off"
+  # end
+end
