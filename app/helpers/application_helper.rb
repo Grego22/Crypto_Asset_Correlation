3efe87991768c78bff4cc4dee671b367e5874442
Greg Garville Jr <gjgarvil@gmail.com>
2018-02-13 12:44:32 -0500

    set dynamic title within app_controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def copyright_generator
-    # DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    # DevcampViewTool::Renderer.copyright 'Greg Garville', 'All rights reserved'
   end
 end
