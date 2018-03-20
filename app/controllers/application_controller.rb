1798f9515da0d4ec5888f97f72649827a42d1756
Greg Garville Jr <gjgarvil@gmail.com>
2018-03-20 11:22:36 -0400

    beginning steps to add guest_user to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include DeviseWhiteList
+  include SetSource
 
+  def current_user
+    
+  end
 
   before_action :set_page_defaults
   def set_page_defaults
